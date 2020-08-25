b586033307958a909fd9341124c4254a72b8e780
Alli Jacobs <jacobsa20@up.edu>
2020-08-24 22:06:17 -0700

    working on User_DAO functionality

--- src/main/java/com/revature/daos/User_DAO.java
+++ src/main/java/com/revature/daos/User_DAO.java
@@ -4,16 +4,36 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.models.User;
+import com.revature.models.User_Role;
 import com.revature.utils.Connection_Util;
 
 public class User_DAO implements IUser_DAO {
-
+	
 	@Override
 	public List<User> findAll() {
-		// TODO Auto-generated method stub
+		try (Connection conn = Connection_Util.getConnection()) {
+			String sql = "SELECT * FROM ers_users;";
+			Statement statement = conn.createStatement();
+			List<User> list = new ArrayList<>();
+			ResultSet rs = statement.executeQuery(sql);
+			while (rs.next()) {
+				User u = new User();
+				u.setId(rs.getInt("ers_users_id"));
+				u.setUsername(rs.getString("ers_username"));
+				u.setPassword(rs.getString("ers_password"));
+				u.setFirst(rs.getString("user_first_name"));
+				u.setLast(rs.getString("user_last_name"));
+				u.setEmail(rs.getString("user_email"));
+			}
+			return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
@@ -38,15 +58,15 @@ public class User_DAO implements IUser_DAO {
 			ps.setString(1, username);
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
-				
-				//keep this in order!
+
+				// keep this in order!
 				u.setId(rs.getInt("ers_users_id"));
 				u.setUsername(rs.getString("ers_username"));
 				u.setPassword(rs.getString("ers_password"));
 				u.setFirst(rs.getString("user_first_name"));
 				u.setLast(rs.getString("user_last_name"));
 				u.setEmail(rs.getString("user_email"));
-								
+
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -63,7 +83,25 @@ public class User_DAO implements IUser_DAO {
 
 	@Override
 	public boolean addUser(User u) {
-		// TODO Auto-generated method stub
+		try(Connection conn= Connection_Util.getConnection()){
+			String sql = "INSERT INTO ers_user (ers_username, ers_password, user_first_name, user_last_name, user_email)"
+					+"VALUES (?,?,?,?,?);";
+			PreparedStatement ps= conn.prepareStatement(sql);
+			int index = 0;
+			ps.setString(++index, u.getUsername());
+			ps.setString(++index, u.getPassword());
+			ps.setString(++index, u.getFirst());
+			ps.setString(++index, u.getLast());
+			ps.setString(++index, u.getEmail());
+//			if(u.getUserRoleId()!=0) {
+//				User_Role ur= u.getUserRoleId();
+//			}else {
+//				ps.setInt(++index, 0);
+//			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
